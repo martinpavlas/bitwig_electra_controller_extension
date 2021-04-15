1c568630995dbb309bb19b903ba1a28335ea6b7e
Anders Skibsted <anders.skibsted@gmail.com>
2021-04-15 11:06:02 +0200

    further clean up

--- Electra2/src/main/java/com/electra/Electra2Extension.java
+++ Electra2/src/main/java/com/electra/Electra2Extension.java
@@ -51,10 +51,6 @@ public class Electra2Extension extends ControllerExtension
      
       //Create hardware knobs
       AbsoluteHardwareKnob [] knob = new AbsoluteHardwareKnob[37];
-      /*
-      final MidiOut outPort2 = host.getMidiOutPort(1);
-      final MidiOut outPort = host.getMidiOutPort(0);
-      */
       for( int i=0; i<37; i++) {
     	  knob[i] = this.hardwareSurface.createAbsoluteHardwareKnob("ABS_KNOB"+i);
     	  knob[i].setAdjustValueMatcher(inPort1.createAbsoluteCCValueMatcher(0, i));
@@ -71,62 +67,9 @@ public class Electra2Extension extends ControllerExtension
       cursorDevice = cursorTrack.createCursorDevice("ELECTRA_CURSOR_DEVICE", "Cursor Device", 0, CursorDeviceFollowMode.FOLLOW_SELECTION);
       
       final Devices devices = new Devices(cursorDevice);
-      //devices.setupAllDevices(cursorDevice);
+      
       SendBank sendBank1 = cursorTrack.sendBank(); 
-     /*
-      final SpecificPluginDevice fabQ3 = cursorDevice.createSpecificVst3Device("72C4DB717A4D459AB97E51745D84B39D");
-      final SpecificPluginDevice fabC2 = cursorDevice.createSpecificVst3Device("79F415E3C8E74807AD5DA3CF7024F618");
-      final SpecificBitwigDevice eClap = cursorDevice.createSpecificBitwigDevice(eClapID);
-      final SpecificBitwigDevice fm4 = cursorDevice.createSpecificBitwigDevice(fm4ID);
-      
-      /*
-      HashMap<String,SpecificPluginDevice> devicesVST3 = new HashMap<String,SpecificPluginDevice>();
-      devicesVST3.put("FabFilter Pro-Q 3", cursorDevice.createSpecificVst3Device("72C4DB717A4D459AB97E51745D84B39D"));
-      devicesVST3.put("FabFilter Pro-C 3", cursorDevice.createSpecificVst3Device("79F415E3C8E74807AD5DA3CF7024F618"));
-     
-      HashMap<String,SpecificBitwigDevice> devicesBitwig = new HashMap<String,SpecificBitwigDevice>();
-      devicesBitwig.put("E-Clap", cursorDevice.createSpecificBitwigDevice(UUID.fromString("7a0a94df-3aa4-4bb5-8e24-2511999871ad")));
-      devicesBitwig.put("FM-4", cursorDevice.createSpecificBitwigDevice(UUID.fromString("7a0a94df-3aa4-4bb5-8e24-2511999871ad")));
-      */
-      /*HashMap<String, SpecificBitwigDevice> devicesBitwig = new HashMap<String,SpecificBitwigDevice>();
-      devicesBitwig = bitwigDeviceCreator(cursorDevice);
-      HashMap<String,SpecificPluginDevice> devicesVST3 = new HashMap<String,SpecificPluginDevice>();
-      devicesVST3 = vst3Creator(cursorDevice);
-      */
-      
-      /*
-      //plugin parameters
-      Parameter [] fabQ3Params = new Parameter[100];
-      Parameter [] fabC2Params = new Parameter[100];
-      Parameter [] eClapParams = new Parameter[100];
-      Parameter [] fm4Params = new Parameter[100];
-      
-      
-      //final SpecificPluginDevice fabQ32 = devices.getVST3(DeviceNames.FABFILTER_PRO_Q3);
-      for(int i=0; i<100; i++){
-    	  fabQ3Params[i] = devices.getVST3(DeviceNames.FABFILTER_PRO_Q3).createParameter(i);
-    	  fabC2Params[i] = devices.getVST3(DeviceNames.FABFILTER_PRO_C2).createParameter(i);
-    	  
-      };
-      /*
-      eClapParams[0] = devices.getBitwigDevice("E-Clap").createParameter("DECAY");
-      eClapParams[1] = devices.getBitwigDevice("E-Clap").createParameter("REPEAT_TIME");
-      eClapParams[2] = devices.getBitwigDevice("E-Clap").createParameter("REPEAT_DURATION");
-      eClapParams[3] = devices.getBitwigDevice("E-Clap").createParameter("WIDTH");
-      eClapParams[4] = devices.getBitwigDevice("E-Clap").createParameter("FREQ");
-      eClapParams[5] = devices.getBitwigDevice("E-Clap").createParameter("Q");
-      eClapParams[6] = devices.getBitwigDevice("E-Clap").createParameter("VELOCITY_SENSITIVITY");
-      eClapParams[7] = devices.getBitwigDevice("E-Clap").createParameter("OUTPUT");
-      
-      fm4Params[0] = devices.getBitwigDevice("FM-4").createParameter("OP1_RATIO");
-      fm4Params[1] = devices.getBitwigDevice("FM-4").createParameter("OP2_RATIO");
-      fm4Params[2] = devices.getBitwigDevice("FM-4").createParameter("OP3_RATIO");
-      fm4Params[3] = devices.getBitwigDevice("FM-4").createParameter("OP4_RATIO");
-      fm4Params[4] = devices.getBitwigDevice("FM-4").createParameter("AEG_ATTACK");
-      fm4Params[5] = devices.getBitwigDevice("FM-4").createParameter("AEG_DECAY");
-      fm4Params[6] = devices.getBitwigDevice("FM-4").createParameter("AEG_SUSTAIN");
-      fm4Params[7] = devices.getBitwigDevice("FM-4").createParameter("AEG_RELEASE");
-      */
+
       for(int i=0; i<32; i++) {
     	  
     	  int parameterNumber = ParameterMappings.fabQ3[i];
@@ -134,15 +77,9 @@ public class Electra2Extension extends ControllerExtension
     	  parameterNumber = ParameterMappings.fabC2[i];
     	  devices.getParameter(DeviceNames.FABFILTER_PRO_C2, parameterNumber).addBinding(knob[i]);
     	  
-    	  //[parameterNumber].addBinding(knob[i+1]);
-      }
-      /*
-      for(int i=0; i<32; i++) {
     	  
-    	  int parameterNumber = ParameterMappings.fabC2[i];
-    	  fabC2Params[parameterNumber].addBinding(knob[i+1]);
       }
-      */
+      
       for(int i=0; i<8; i++) {
     	  
     	  int parameterNumber = ParameterMappings.eClap[i];
@@ -217,12 +154,7 @@ public class Electra2Extension extends ControllerExtension
 
 
 
-private void midiReceived(ShortMidiMessage msg) {
-	   
-	  
-   }
-
-@Override
+	@Override
    public void exit()
    {
 		
@@ -236,28 +168,7 @@ private void midiReceived(ShortMidiMessage msg) {
 		   this.hardwareSurface.updateHardware(); 
 	   
    }
-   /*
-   public HashMap<String, SpecificBitwigDevice> bitwigDeviceCreator (PinnableCursorDevice cursorD){
-	   
-	   HashMap<String,SpecificBitwigDevice> out = new HashMap<String,SpecificBitwigDevice>();
-	   out.put("E-Clap", cursorDevice.createSpecificBitwigDevice(UUID.fromString("7a0a94df-3aa4-4bb5-8e24-2511999871ad")));
-	   out.put("FM-4", cursorDevice.createSpecificBitwigDevice(UUID.fromString("7a0a94df-3aa4-4bb5-8e24-2511999871ad")));
-	   
-	   return out;
-	   
-   }
-   
-   public HashMap<String, SpecificPluginDevice> vst3Creator (PinnableCursorDevice cursorD){
-	   
-	      
-	      HashMap<String,SpecificPluginDevice> out = new HashMap<String,SpecificPluginDevice>();
-	      out.put("FabFilter Pro-Q 3", cursorDevice.createSpecificVst3Device("72C4DB717A4D459AB97E51745D84B39D"));
-	      out.put("FabFilter Pro-C 2", cursorDevice.createSpecificVst3Device("79F415E3C8E74807AD5DA3CF7024F618"));
-	      
-	      return out;
-	   
-   }
-   */
+
 
 
 
